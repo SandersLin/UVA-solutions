1c50b38c0e761868db948548f4bfb597a1c9a136
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2019-02-19 20:47:25 +0800

    Create Back to High School Physics.cpp

---
+++ Back to High School Physics.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+
+int main(){
+
+	freopen("input.txt", "r",stdin);
+	freopen("output.txt", "w",stdout);
+	
+
+	int a,  b;
+	while(cin >> a >> b){
+		cout << a*b*2 << endl; 
+	} 
+
+	
+
+	return 0;
+}
+
